98a88c3b7b162e6b2098b5f88966c77008635ee3
zhy-g <1926963751@qq.com>
2019-10-13 21:50:05 +0800

    This is new file

---
+++ Eighth-Todolist/js/main.js
@@ -0,0 +1,76 @@
+;(function(){
+    'use strick';
+    function copy(obj){
+        return Object.assign({},obj);
+    }
+
+    new Vue({
+        el:'#main',
+        data:{
+            list:[],
+            current:{}
+        },
+        mounted: function(){
+            this.list = msg.get('list') || this.list;
+        },
+
+        methods:{
+            add:function(){
+                var is_update,id;
+                
+                var is_update = id = this.current.id;
+                if(is_update){
+                    var index = this.find_index(id);
+
+                    Vue.set(this.list,index,copy(this.current));
+
+                    console.log('this.list :', this.list);
+
+                }else{
+                    var title = this.current;
+                    if(!title && title !== 0) return;
+                    var todo = copy(this.current);
+                    
+                    todo.id = this.next_id();
+
+                    this.list.push(todo);
+
+                    console.log('this.list :', this.list);
+                }
+                
+                this.reset_current();
+            },
+
+            remove: function(id){
+                var index = this.find_index(id);
+                this.list.splice(index,1); 
+            },
+            next_id: function(){
+                return this.list.length +1;
+            },
+            set_current: function(todo){
+                this.current = copy(todo);
+            },
+            reset_current: function(){
+              this.set_current({});  
+            },
+            find_index:function(id){
+                return this.list.findIndex(function(item){
+                    return item.id = id;
+                })
+            }
+        },
+        watch:{
+            list:{
+                deep:true,
+                handler: function(new_val, old_val){
+                    if(new_val){
+                        msg.set('list', new_val);
+                    }else{
+                        msg.set('list',[]);
+                    }
+                }
+            }
+        }
+    })
+})();
---
+++ Eighth-Todolist/js/myStorage.js
@@ -0,0 +1,17 @@
+;(function(){
+    window.msg = {
+        set: set,
+        get:get,
+    }
+
+    //封装localStorage
+    function set(key, val){
+        localStorage.setItem(key,JSON.stringify(val));
+    }
+    function get (key){
+        var json = localStorage.getItem(key);
+        if(json){
+            return JSON.parse(json);
+        }
+    }
+})();
