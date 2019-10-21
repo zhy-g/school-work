af9fc4510ef1d4fa87cfa6012602710f6ca92945
zhy-g <1926963751@qq.com>
2019-10-21 11:25:20 +0800

    first node file

---
+++ Node/main.js
@@ -0,0 +1,13 @@
+//阻塞代码示例
+    // var fs = require("fs");
+    // var data = fs.readFileSync('input.txt');
+    // console.log(data.toString());
+    // console.log("程序执行结束");
+// 非阻塞代码示例
+    var fs = require("fs");
+    fs.readFile("input.txt", function(err, data){
+        if(err) return console.error(err);
+        console.log(data.toString());
+    });
+    
+    console.log('程序执行结束！');
